335057df07dcb157000c07978c4166849dcc77f2
Eric <ericos.bennett@gmail.com>
2021-03-15 17:14:05 +0000

    add placeholder location markers to map

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -9,8 +9,15 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-// initial location on map
+// TEST VARIABLES
 const position = [49.2827, -123.1207];
+
+const locations = [
+  [49.275865, -123.152283],
+  [49.251903, -123.124281],
+  [49.329947, -122.949554],
+]
+
 
 const Map = props => {
 
@@ -23,6 +30,9 @@ const Map = props => {
         attribution='&copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      { locations.map(item => (
+        <Marker position={item} />
+      ))}
     </MapContainer>
   );
 };
