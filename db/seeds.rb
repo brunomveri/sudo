a73b137f60aefac9418c15d3e40b3b0745f331de
loloffs <lukeoloffs@gmail.com>
2021-03-15 17:35:57 +0000

    Locations seeds/schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Location.create!(
+  title:  'Kitsilano Beach Basketball Court',
+  description: 'Outdoor basketball court with beauftiul backdrop next to the ocean',
+  latitude: 49.275865,
+  longitude: -123.152283,
+  image: "https://i.pinimg.com/originals/9a/fa/0a/9afa0aff5b7bb1e41a3b4de509a83921.jpg"
+)
+Location.create!(
+  title:  'Douglas Park Running Track',
+  description: 'A track that circles a large park in a lovely neighbourhood',
+  latitude: 49.251903,
+  longitude: -123.124281,
+  image: "https://vancouver.ca//parks/parks/images/douglas01.jpg"
+)
+Location.create!(
+  title:  'Quarry Rock',
+  description: 'A scenic hike close to great coffee and food',
+  latitude: 49.329947,
+  longitude: -122.949554,
+  image: "https://www.vancouvertrails.com/images/hikes-small/quarry-rock.jpg"
+)
