ed536fa9e7e795ea522091d341a33d1f404c8043
Eric <ericos.bennett@gmail.com>
2021-03-15 18:49:28 +0000

    add location pins

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -10,12 +10,24 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 // TEST VARIABLES
-const position = [49.2827, -123.1207];
+const mapPosition = [49.2827, -123.1207];
 
 const locations = [
-  [49.275865, -123.152283],
-  [49.251903, -123.124281],
-  [49.329947, -122.949554],
+  {
+    title: 'Test 1',
+    description: 'Description 1',
+    coordinates: [49.275865, -123.152283]
+  },
+  {
+    title: 'Test 2',
+    description: 'Description 2',
+    coordinates: [49.251903, -123.124281]
+  },
+  {
+    title: 'Test 3',
+    description: 'Description 3',
+    coordinates: [49.329947, -122.949554]
+  }
 ]
 
 
@@ -25,13 +37,18 @@ const Map = props => {
   
   return (
     <MapContainer
-      center={position} zoom={13} className={classes.root}>
+      center={mapPosition} zoom={13} className={classes.root}>
       <TileLayer
         attribution='&copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       { locations.map(item => (
-        <Marker position={item} />
+        <Marker position={item.coordinates}>
+          <Popup>
+            <h1>{item.title}</h1>
+            <p>{item.description}</p>
+          </Popup>
+        </Marker>
       ))}
     </MapContainer>
   );
