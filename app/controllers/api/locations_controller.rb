e182b485af7fbd5f2a8a08e96dcdaa6cb57a2385
Eric <ericos.bennett@gmail.com>
2021-03-17 11:39:27 -0700

    add create route to locations

--- app/controllers/api/locations_controller.rb
+++ app/controllers/api/locations_controller.rb
@@ -3,4 +3,19 @@ class Api::LocationsController < ApplicationController
     @locations = Location.all
     render json: @locations
   end
+  
+  def create
+
+    @activity = Activity.find params[:activity_id]
+
+    @activity.locations.create!(
+      title: params[:title],
+      description: params[:description],
+      latitude: params[:latitude],
+      longitude: params[:longitude],
+      image: params[:image]
+    )
+
+  end
+
 end
