9fbe9524472083177a51f83f0c7e03725ce0348a
Eric <ericos.bennett@gmail.com>
2021-03-17 10:59:48 -0700

    add new users + favourites routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
 
   namespace :api do
-    resources :locations, only: [:index]
+    resources :locations, only: [:index, :new, :create]
     resources :activities, only: [:index]
+    resources :users, only: [:new] do
+      resources :favourites, except: [:update]
+    end
   end
 
 end
