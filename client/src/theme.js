02c556c699db125ad9465e04296948934f72b017
loloffs <lukeoloffs@gmail.com>
2021-03-20 21:33:46 +0000

    Sidebar changes

--- client/src/theme.js
+++ client/src/theme.js
@@ -2,6 +2,9 @@ import { createMuiTheme } from '@material-ui/core/styles' ;
 
 export default createMuiTheme({
   typography: {
+    loggedInUser: {
+      fontFamily: 'Comfortaa'
+    },
     h6: {
       fontFamily: 'Comfortaa'
     }
