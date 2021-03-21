22b9cb5109a124751422802f1de27794a768fbab
Eric <ericos.bennett@gmail.com>
2021-03-21 20:19:46 +0000

    refactor logout button

--- client/src/components/LogoutButton.jsx
+++ client/src/components/LogoutButton.jsx
@@ -1,70 +1,28 @@
 import React from 'react';
-import { createMuiTheme, withStyles, makeStyles, ThemeProvider } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-import { green, purple } from '@material-ui/core/colors';
-
-const BootstrapButton = withStyles({
-  root: {
-    boxShadow: 'none',
-    textTransform: 'none',
-    fontSize: 16,
-    padding: '6px 12px',
-    border: '1px solid',
-    lineHeight: 1.5,
-    backgroundColor: '#0063cc',
-    borderColor: '#0063cc',
-    fontFamily: [
-      '-apple-system',
-      'BlinkMacSystemFont',
-      '"Segoe UI"',
-      'Roboto',
-      '"Helvetica Neue"',
-      'Arial',
-      'sans-serif',
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(','),
-    '&:hover': {
-      backgroundColor: '#0069d9',
-      borderColor: '#0062cc',
-      boxShadow: 'none',
-    },
-    '&:active': {
-      boxShadow: 'none',
-      backgroundColor: '#0062cc',
-      borderColor: '#005cbf',
-    },
-    '&:focus': {
-      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.5)',
-    },
-  },
-})(Button);
-
-const ColorButton = withStyles((theme) => ({
-  root: {
-    color: theme.palette.getContrastText(purple[500]),
-    backgroundColor: '#72acb1',
-    '&:hover': {
-      backgroundColor: '#8a9098',
-    },
-  },
-}))(Button);
 
 const useStyles = makeStyles((theme) => ({
-  margin: {
+  button: {
     margin: theme.spacing(1),
+    backgroundColor: '#72acb1',
+    '&:hover': {
+      backgroundColor: '#72acb1',
+      filter: 'brightness(0.7)',
+    }
   },
 }));
 
 export default function CustomizedButtons() {
+ 
   const classes = useStyles();
 
   return (
     <div>
-      <ColorButton variant="contained" color="primary" className={classes.margin}>
+      <Button variant="contained" color="primary" className={classes.button}>
         Logout
-      </ColorButton>
+      </Button>
     </div>
   );
+
 }
